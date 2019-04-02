d04daac2e55a2697f084b88d392bb1dc6caa8c18
satishsarin <sarin.satish@gmail.com>
2019-04-02 18:48:14 +0530

    added enzyme

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,18 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { shallow } from 'enzyme';
 import App from './App';
+import UserForm from './components/UserForm';
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
-});
+describe('<App/>', () => {
+
+	it('renders without crashing', () => {
+	  shallow(<App />);
+	});
+
+	it('render one <userForm/> compoenent', () => {
+	  const component = shallow(<App />);
+	  expect(component.find(UserForm)).toHaveLength(1);
+	});
+
+
+})
